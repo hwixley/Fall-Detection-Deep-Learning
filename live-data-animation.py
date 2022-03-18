34e735bf125ec5c770c8dda716638394ccfcc9e6
Harry Wixley <hwixley1@gmail.com>
2022-03-18 15:23:13 +0000

    animation worked :)

--- live-data-animation.py
+++ live-data-animation.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import matplotlib.pylab as pl
 from celluloid import Camera
 from IPython.display import Video
+from tqdm import tqdm
 
 def loadNpy(filename):
     with open(os.getcwd() + "/train-val-test/"+ filename, "rb") as f: return np.load(f)
@@ -29,12 +30,29 @@ fig, ax = plt.subplots(figsize=(4, 4))
 camera=Camera(fig)#Make a camera of the figure
 
 plt.cla()
+plt.yticks(np.linspace(0, 1, 10))
 ecg_data = []
 label_data = []
+label_plots = []
+label_colours = []
 
-for window in range(10):
+for window in tqdm(range(10)):
     sample = data[window, :, :]
     label = y_train[window]
+
+    #label_data.append(label)
+    if label == 1 and len(label_data) > 0 and label_data[-1] == 0:
+        plot_data = [(100/13)*len(ecg_data)]*2
+        #ax.plot(plot_data, [0,1], c="g")
+        label_plots.append(plot_data)
+        label_colours.append("g")
+        
+
+    if label == 0 and len(label_data) > 0 and label_data[-1] == 1:
+        plot_data = [(100/13)*len(ecg_data)]*2
+        #ax.plot(plot_data, [0,1], c="g")
+        label_plots.append(plot_data)
+        label_colours.append("r")
 
     label_data.append(label)
 
@@ -46,10 +64,12 @@ for window in range(10):
             max_val = (100/13)*len(ecg_data)
             t_list = np.linspace(0, max_val, len(ecg_data))
             #Our animation will have each frame be a different value of k from the list above    
-            ax.plot(t_list, ecg_data, c="red")
+            ax.plot(t_list, ecg_data, c="b")
 
-            llist = np.linspace(0, max_val, len(label_data))
-            ax.plot(llist, label_data, c="b")
+            for i, plot in enumerate(label_plots):
+                ax.plot(plot, [0,1], c=label_colours[i])
+            #llist = np.linspace(0, max_val, len(label_data))
+            #ax.plot(llist, label_data, c="b")
             #ax.set_xticks(t_list)
             #For a given value of k, we'll plot our function with that k-value   
             camera.snap()
